31fafeba8aab928ab40679b7a54ecc5765a8f868
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2025-02-16 13:22:40 +0100

    Ajout commentaires

--- src/main/C/client.c
+++ src/main/C/client.c
@@ -13,18 +13,14 @@
 #define NB_CLIENTS 5
 #define NB_GUICHET 0
 
-
 #include "../ressources/codeC/def.h"
 #include "client.h"
-/**
-*    @brief Fonction qui simule le client
-*
-*/
+
 void simulation(int ids) {
     entrer(0);
     delai(5, 1);   // 0 < delta < temps < 100
     transfert(0, 1);
-    delai(6, 2);   // 0 < delta < temps < 100
+    delai(6, 2);
     transfert(1, 2);
 }
 
--- src/main/C/client.h
+++ src/main/C/client.h
@@ -1,8 +1,36 @@
+/**
+ * @file client.h
+ * @author RESENDE Nicolas et BURNEL Mathias
+ * @brief Bibliothèque de client.c
+ * @version 0.1
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #ifndef CLIENT_H
 #define CLIENT_H
 
+/**
+ * @brief Fonction qui récupère le nombre d'étapes du monde
+ * 
+ * @return int Le nombre d'étapes du monde
+ */
 int get_nb_etapes();
+
+/**
+ * @brief Fonction qui récupère le nombre de guichets du monde
+ * 
+ * @return int Le nombre de guichets du monde
+ */
 int get_nb_guichets();
+
+/**
+ * @brief Fonction qui récupère le nombre de clients du monde
+ * 
+ * @return int Le nombre de clients du monde
+ */
 int get_nb_clients();
 
 #endif
