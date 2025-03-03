588ae2fdb9fbd9fd3d4c4dd4ba84969198acade7
burnel9u <mathias.burnel9@etu.univ-lorraine.fr>
2025-03-03 18:02:23 +0100

    ajout KitC

---
+++ src/main/java/twisk/outils/KitC.java
@@ -0,0 +1,31 @@
+package twisk.outils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class KitC {
+    public void creerEnvironment(){
+
+        Path directory = Paths.get("/tmp/twisk");
+        try {
+            // création du répertoire twisk sous /tmp.
+            // Ne déclenche pas d’erreur si le répertoire existe déjà
+
+             Files.createDirectories(directory);
+            // copie des fichiers programmeC.o et def.h sous /tmp/twisk
+            String[] liste = {"programmeC.o", "def.h"};
+            for (String nom : liste) {
+                InputStream src = getClass().getResourceAsStream("/codeC/" + nom);
+                Path dest = directory.resolve(nom);
+                Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+            }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+}
