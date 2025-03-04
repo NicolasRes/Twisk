fdb979b13d5cd870aeaeb5f63cfb59dac84036c6
burnel9u <mathias.burnel9@etu.univ-lorraine.fr>
2025-03-04 22:54:19 +0100

    Test toC

--- src/main/tests/Java/TestEtape.java
+++ src/main/tests/Java/TestEtape.java
@@ -2,9 +2,7 @@ package Java;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import twisk.monde.Activite;
-import twisk.monde.Etape;
-import twisk.monde.Guichet;
+import twisk.monde.*;
 
 import java.util.Iterator;
 
@@ -13,45 +11,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestEtape {
     private Etape e;
     private Etape gui;
-    private Etape act;
+    private Etape act1;
     private Etape gui2;
     private Etape act2;
+    private SasEntree sasEntree;
+    private SasSortie sasSortie;
 
     @BeforeEach
     void setUp() {
         e = new Guichet("A",2);
         gui = new Guichet("B",2);
-        act = new Activite("C",5,2);
+        act1 = new Activite("C",5,2);
         gui2 = new Guichet("D",2);
         act2 = new Activite("E",6,2);
+        sasEntree = new SasEntree();
+        sasSortie = new SasSortie();
+
     }
 
     @Test
     void ajouterSuccesseur() {
-        e.ajouterSuccesseur(gui, act);
+        e.ajouterSuccesseur(gui, act1);
         gui.ajouterSuccesseur(gui2);
 
         assertEquals(2, e.nbSuccesseur());
         assertEquals(gui, e.getSuccesseur(0));
-        assertEquals(act, e.getSuccesseur(1));
+        assertEquals(act1, e.getSuccesseur(1));
         assertEquals(1, gui.nbSuccesseur());
     }
 
     @Test
     void estUneActivite() {
         assertFalse(gui.estUneActivite());
-        assertTrue(act.estUneActivite());
+        assertTrue(act1.estUneActivite());
     }
 
     @Test
     void estUnGuichet() {
         assertTrue(gui.estUnGuichet());
-        assertFalse(act.estUnGuichet());
+        assertFalse(act1.estUnGuichet());
     }
 
     @Test
     void iterator() {
-        e.ajouterSuccesseur(gui, act);
+        e.ajouterSuccesseur(gui, act1);
 
         Iterator<Etape> i = e.iterator();
 
@@ -61,19 +64,19 @@ class TestEtape {
 
         // Deuxième élément suivant
         assertTrue(i.hasNext(), "L'itérateur doit avoir un deuxième élément suivant");
-        assertEquals(i.next(), act);
+        assertEquals(i.next(), act1);
 
         assertFalse(i.hasNext(), "L'itérateur ne doit pas avoir d'élément suivant");
     }
 
     @Test
     void testToString() {
-        e.ajouterSuccesseur(gui, act);
-        gui.ajouterSuccesseur(act);
+        e.ajouterSuccesseur(gui, act1);
+        gui.ajouterSuccesseur(act1);
 
         assertEquals("A : 2 successeur -> B, C", e.toString());
         assertEquals("B : 1 successeur -> C", gui.toString());
-        assertEquals("C : 0 successeur -> ", act.toString());
+        assertEquals("C : 0 successeur -> ", act1.toString());
     }
 
     @Test
@@ -86,8 +89,11 @@ class TestEtape {
     @Test
     void testToC() {
 
-        act.ajouterSuccesseur(act2);
-        System.out.println(act.toC());
+        sasEntree.ajouterSuccesseur(act1);
+        act1.ajouterSuccesseur(act2);
+        act2.ajouterSuccesseur(sasSortie);
+
+        System.out.println(sasEntree.toC());
 
 
     }
