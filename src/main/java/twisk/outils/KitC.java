9261cdfbd6bc892c73dff3441ea35c0258691c17
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2025-03-05 21:02:42 +0100

    Ajout kitC et méthode creerEnvironnement()

--- src/main/java/twisk/outils/KitC.java
+++ src/main/java/twisk/outils/KitC.java
@@ -8,24 +8,26 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class KitC {
-    public void creerEnvironment(){
 
+    /**
+     * Méthode qui crée des copies de fichiers dans un répertoire temporaire en vue d'intégrer du code C dans Java
+     */
+    public void creerEnvironnement(){
         Path directory = Paths.get("/tmp/twisk");
         try {
             // création du répertoire twisk sous /tmp.
             // Ne déclenche pas d’erreur si le répertoire existe déjà
-
              Files.createDirectories(directory);
-            // copie des fichiers programmeC.o et def.h sous /tmp/twisk
-            String[] liste = {"programmeC.o", "def.h"};
+
+            // copie des fichiers programmeC.o, def.h et programmeCMac.o sous /tmp/twisk
+            String[] liste = {"programmeC.o", "def.h", "programmeCMac.o"};
             for (String nom : liste) {
                 InputStream src = getClass().getResourceAsStream("/codeC/" + nom);
                 Path dest = directory.resolve(nom);
                 Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
             }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
