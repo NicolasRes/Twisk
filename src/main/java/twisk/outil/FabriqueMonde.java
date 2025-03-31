1f2f6742dd2f96dc7a4392660ad6700e1be3b17b
resende3u <>
2025-03-31 09:40:11 +0200

    Modif fabrique monde

--- src/main/java/twisk/outil/FabriqueMonde.java
+++ src/main/java/twisk/outil/FabriqueMonde.java
@@ -65,4 +65,31 @@ public class FabriqueMonde {
         return monde;
 
     }
+
+    public static Monde mondeBifurc() {
+        Monde monde = new Monde();
+        Activite act1 = new Activite("étape 1 tobogan",8,3);
+        Guichet g1 = new Guichet("ètape 2 g1",3);
+        Activite act2 = new ActiviteRestreinte("etape 3 piscine",4,2);
+        Guichet g2 = new Guichet("ETAPE 4 g2",6);
+        Activite act3 = new ActiviteRestreinte("A3",2,1);
+        Activite act4 = new Activite("etapebifuc1", 4, 2);
+        Activite act5 = new Activite("etapebifuc2", 3, 2);
+
+        act1.ajouterSuccesseur(g1);
+        g1.ajouterSuccesseur(act2);
+        act2.ajouterSuccesseur(act5);
+        act2.ajouterSuccesseur(g2);
+        g2.ajouterSuccesseur(act3);
+        act3.ajouterSuccesseur(act4);
+        act4.ajouterSuccesseur(act5);
+
+        monde.ajouter(act1,g1,act2, g2, act3, act4, act5);
+
+        monde.aCommeEntree(act1);
+        monde.aCommeEntree(act4);
+        monde.aCommeSortie(act5);
+
+        return monde;
+    }
 }
