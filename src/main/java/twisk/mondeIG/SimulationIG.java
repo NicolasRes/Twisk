2b03fe5f936cec0aeca0adf357ce9d325b952ad9
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2025-04-21 00:11:59 +0200

    Ajustements et commentaires

--- src/main/java/twisk/mondeIG/SimulationIG.java
+++ src/main/java/twisk/mondeIG/SimulationIG.java
@@ -29,38 +29,17 @@ public class SimulationIG {
     private void verifierMondeIG() {
 
         for(EtapeIG e : this.monde) {
-            if (e.getType().equals("Guichet")) {
+            if(e.getType().equals("Guichet")) {
                 guichetSansSuccesseur(e);
                 succGuichetValide(e);
                 guichetUnSuccesseur(e);
                 guichetPasQueSortie(e);
                 identifierActiviteRestreinte(e);
 
-            }else{
+            }
+            else {
                 activiteVerifNbSuccPred(e);
             }
-        }
-    }
-
-    private void activiteVerifNbSuccPred(EtapeIG e) {
-        if(e.estEntree() && (e.getPredecesseurs().isEmpty())){
-            System.out.println("Erreur, l'activité " + e.getNom() + " est une entrée, donc elle doit avoir au moins un prédecesseur");
-        }
-        
-        if(e.estSortie() && (e.getSuccesseurs().isEmpty())){
-            System.out.println("Erreur, l'activité " + e.getNom() + " est une sortie, donc elle doit avoir au moins un successeur");
-        }
-    }
-
-    private void guichetPasQueSortie(EtapeIG e) {
-        if(!e.estSortie() || !e.estEntree()){
-            System.out.println("Erreur, le guichet "+ e.getNom() + "doit être une Entrée/ Sortie");
-        }
-    }
-
-    public void guichetUnSuccesseur(EtapeIG e){
-        if(e.getSuccesseurs().size()!=1){
-            System.out.println("Erreur, le guichet " + e.getNom() + " a un nombre de successeurs incorrecte, il doit avoir un seul successeur.");
         }
     }
 
@@ -78,7 +57,7 @@ public class SimulationIG {
      * @param e Le guichet dont on examine le type du successeur
      */
     private void succGuichetValide(EtapeIG e) {
-        EtapeIG succ = e.getSuccesseurs().getFirst();
+        EtapeIG succ = e.premierSuccesseur(e);
         if(!succ.getType().equals("Activite")) {
             System.out.println("Erreur, le successeur du guichet " + e.getNom() + " doit forcément être une activité");
         }
@@ -89,12 +68,46 @@ public class SimulationIG {
      * @param e L'étape (guichet) dont le successeur doit être identifié comme activité restreinte
      */
     private void identifierActiviteRestreinte(EtapeIG e) {
-        EtapeIG succ = e.getSuccesseurs().getFirst();   // On récupère le successeur du guichet (une activité)
+        EtapeIG succ = e.premierSuccesseur(e);   // On récupère le successeur du guichet (une activité)
         if(succ.getType().equals("Activite")) {
             ActiviteIG act = (ActiviteIG) succ;
             if(!act.estActiviteRestreinte()) {  // Si l'activité n'est pas déjà une activité restreinte, on la définie comme telle
                 act.switchActiviteRestreinte();
             }
+        }
+    }
+
+    /**
+     * Méthode qui envoie un messsage d'erreur si une étape a une entrée / une sortie sans avoir de prédecesseur / successeur
+     * @param e L'étape à vérifier
+     */
+    private void activiteVerifNbSuccPred(EtapeIG e) {
+        if(e.estEntree() && (e.getPredecesseurs().isEmpty())){
+            System.out.println("Erreur, l'activité " + e.getNom() + " est une entrée, donc elle doit avoir au moins un prédécesseur");
+        }
+
+        if(e.estSortie() && (e.getSuccesseurs().isEmpty())){
+            System.out.println("Erreur, l'activité " + e.getNom() + " est une sortie, donc elle doit avoir au moins un successeur");
+        }
+    }
+
+    /**
+     * Méthode qui envoie un messsage d'erreur si un guichet est bien une entrée et une sortie
+     * @param e Le guichet à vérifier
+     */
+    private void guichetPasQueSortie(EtapeIG e) {
+        if(!e.estSortie() || !e.estEntree()){
+            System.out.println("Erreur, le guichet "+ e.getNom() + "doit être une Entrée / Sortie");
+        }
+    }
+
+    /**
+     * Méthode qui vérifie si un guichet a exactement un seul et unique successeur
+     * @param e Le guichet à vérifier
+     */
+    public void guichetUnSuccesseur(EtapeIG e){
+        if(e.getSuccesseurs().size()!=1){
+            System.out.println("Erreur, le guichet " + e.getNom() + " a un nombre de successeurs incorrect, il doit avoir un seul successeur");
         }
     }
 
