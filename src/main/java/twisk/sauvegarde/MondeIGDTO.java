ab1981aa18693671db9e779f01d71725facd9f70
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2025-05-21 22:32:59 +0200

    Stockage des infos du MondeIG en version DTO pour JSON

--- src/main/java/twisk/sauvegarde/MondeIGDTO.java
+++ src/main/java/twisk/sauvegarde/MondeIGDTO.java
@@ -6,8 +6,22 @@ import java.util.ArrayList;
  * Classe MondeIGDTO qui sert à conserver les données d'un MondeIG pour sa conversion en JSON et sauvegarde
  */
 public class MondeIGDTO {
-    public ArrayList<EtapeIGDTO> etapes = new ArrayList<>();
-    public ArrayList<ArcIGDTO> arcs = new ArrayList<>();
-    public ArrayList<String> entrees = new ArrayList<>();
-    public ArrayList<String> sorties = new ArrayList<>();
+    public ArrayList<EtapeIGDTO> etapes;
+    public ArrayList<ArcIGDTO> arcs;
+    public ArrayList<String> entrees;
+    public ArrayList<String> sorties;
+
+    /**
+     * Constructeur de la classe MondeIGDOT
+     * @param etapes les étapes version dto
+     * @param arcs les arcs version dto
+     * @param entrees les entrées version dto
+     * @param sorties les sorties version dto
+     */
+    public MondeIGDTO(ArrayList<EtapeIGDTO> etapes, ArrayList<ArcIGDTO> arcs, ArrayList<String> entrees, ArrayList<String> sorties) {
+        this.etapes = etapes;
+        this.arcs = arcs;
+        this.entrees = entrees;
+        this.sorties = sorties;
+    }
 }
