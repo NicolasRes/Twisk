fa527ab78e54045c35a5635468ba5e01b02c3e79
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2025-05-21 22:30:59 +0200

    Classe qui sert à stocker les informations des arcs du MondeIG

--- src/main/java/twisk/sauvegarde/ArcIGDTO.java
+++ src/main/java/twisk/sauvegarde/ArcIGDTO.java
@@ -6,4 +6,14 @@ package twisk.sauvegarde;
 public class ArcIGDTO {
     public String identifiantSource;
     public String identifiantDestination;
+
+    /**
+     * Constructeur de la classe ArcIGDTO
+     * @param idSource L'identifiant du PDC source
+     * @param idDest L'identifiant du PDC destination
+     */
+    public ArcIGDTO(String idSource, String idDest) {
+        this.identifiantSource = idSource;
+        this.identifiantDestination = idDest;
+    }
 }
